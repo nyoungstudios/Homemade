d9b66f45639899ef7f57528acd0e4de0be645aef
nyoungstudios <nathanielyoung95070@gmail.com>
2018-11-28 02:20:20 -0500

    adds error handler for file not found

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', hello='Hello World!')
 
+#if wrong url is entered
+@app.errorhandler(404)
+def fourOohFour(error):
+    return "file not found"
 
 if __name__ == '__main__':
     app.run(debug=True)

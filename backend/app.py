187c92d1f1b0f143465517cccb6d1a1065af396d
nyoungstudios <nathanielyoung95070@gmail.com>
2018-11-28 02:17:35 -0500

    adds dynamic content with flask

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', hello='Hello World!')
 
 
 if __name__ == '__main__':

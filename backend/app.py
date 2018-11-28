953cc4c70d689e88e501893630f09a93abf3ca6c
nyoungstudios <nathanielyoung95070@gmail.com>
2018-11-28 01:59:26 -0500

    backend serves webpage

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
-    return app.send_static_file('./../frontend/index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':

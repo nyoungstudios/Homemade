016d70841fac0f2c4bbc9c175b4813dbcd0d151a
nyoungstudios <nathanielyoung95070@gmail.com>
2018-12-03 14:17:35 -0500

    updates template styling for frontend

--- backend/test.py
+++ backend/test.py
@@ -1,10 +1,13 @@
 import FirebaseAuth
 from firebase_admin import auth
 
-user = auth.create_user(
-    email='user@example.com',
-    email_verified=False,
-    password='secretPassword',
-    display_name='John Doe',
-    disabled=False)
-print('Successfully fetched user data: {0}'.format(user.uid))
+# user = auth.create_user(
+#     email='user@example.com',
+#     email_verified=False,
+#     password='secretPassword',
+#     display_name='John Doe',
+#     disabled=False)
+# print('Successfully fetched user data: {0}'.format(user.uid))
+
+user = auth.get_user('Ot43OM7k4mOhmU7QbNSDsEbInvY2')
+print(user.photo_url)

41ec4d54c2fb32808797085d67392f726bf7620a
nyoungstudios <nathanielyoung95070@gmail.com>
2018-12-03 21:10:44 -0500

    updates the form frontend

--- backend/static/signup.js
+++ backend/static/signup.js
@@ -1,3 +1,15 @@
+//javascript file for signup webpage
+
 $(document).ready(function() {
   console.log('hi');
+  
+  //variables selected with jQuery
+  var email1Field = $('#email1');
+  var email2Field = $('#email2');
+  var password1Field = $('#password1');
+  var password2Field = $('#password2');
+                         
+  
+  
+  
 });

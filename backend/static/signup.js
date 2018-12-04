08cf61b6f42db4b295c4a2f2608b54228a60f0d3
nyoungstudios <nathanielyoung95070@gmail.com>
2018-12-03 23:34:52 -0500

    updates to javascript

--- backend/static/signup.js
+++ backend/static/signup.js
@@ -1,15 +1,53 @@
 //javascript file for signup webpage
 
 $(document).ready(function() {
-  console.log('hi');
+  console.log('hi1');
   
-  //variables selected with jQuery
+  //variables selected with jQuery for text fields
+  
+  var firstNameField = $('#firstName');
+  var lastNameField = $('lastName');
   var email1Field = $('#email1');
   var email2Field = $('#email2');
   var password1Field = $('#password1');
   var password2Field = $('#password2');
+  
+  //variables selected with jQuery for error messages
+  var firstNameError = $('#firstNameError');
+  var LastNameError = $('#LastNameError');
+  var email1Error = $('#email1Error');
+  var email2Error = $('#email2Error');
+  var pasword1Error = $('#pasword1Error');
+  var pasword2Error = $('#pasword2Error');
+  
+  
+  $("button[type='submit']").click(function(event) {
+    console.log("test");
+    
+    if ()
+    
+    event.preventDefault();
+    
+  });
+  
+  
+  
                          
-  
-  
-  
 });
+
+//function to hide the error message
+function hideErrorMessage(fieldError) {
+  fieldError.parent().get(0).className = "d-none";
+};
+
+//function to show the error message
+function showErrorMessage(fieldError, newText) {
+  fieldError.get(0).textContent = newText;
+  fieldError.parent().get(0).className = "d-block";
+};
+
+function changeColor(fieldError) {
+  fieldError.get(0).className = "form-text text-danger";
+};
+
+

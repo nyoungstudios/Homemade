311ea45576521b81e5552a1183b19f471b2b2560
nyoungstudios <nathanielyoung95070@gmail.com>
2018-12-03 23:49:40 -0500

    adds more checking logic for forms

--- backend/static/signup.js
+++ backend/static/signup.js
@@ -14,29 +14,73 @@ $(document).ready(function() {
   
   //variables selected with jQuery for error messages
   var firstNameError = $('#firstNameError');
-  var LastNameError = $('#LastNameError');
+  var lastNameError = $('#lastNameError');
   var email1Error = $('#email1Error');
   var email2Error = $('#email2Error');
-  var pasword1Error = $('#pasword1Error');
-  var pasword2Error = $('#pasword2Error');
+  var password1Error = $('#password1Error');
+  var password2Error = $('#password2Error');
   
   
   $("button[type='submit']").click(function(event) {
     console.log("test");
     
     if (firstNameField.val() == '') {
-      event.preventDefault();  
+      event.preventDefault();
+      changeColorRed(firstNameError);
+      hideErrorMessage(lastNameError);
+      hideErrorMessage(email1Error);
+      hideErrorMessage(email2Error);
+      hideErrorMessage(password1Error);
+      hideErrorMessage(password2Error);
+      showErrorMessage(firstNameError, "Please fill out this field.")
     } else if (lastNameField.val() == '') {
       event.preventDefault();
+      changeColorRed(lastNameError);
+      hideErrorMessage(firstNameError);
+      hideErrorMessage(email1Error);
+      hideErrorMessage(email2Error);
+      hideErrorMessage(password1Error);
+      hideErrorMessage(password2Error);
+      showErrorMessage(lastNameError, "Please fill out this field.")
     } else if (email1Field.val() == '') {
       event.preventDefault();
+      changeColorRed(email1Error);
+      hideErrorMessage(firstNameError);
+      hideErrorMessage(lastNameError);
+      hideErrorMessage(email2Error);
+      hideErrorMessage(password1Error);
+      hideErrorMessage(password2Error);
+      showErrorMessage(email1Error, "Please fill out this field.")
     } else if (email2Error.val() == '') {
       event.preventDefault();
+      changeColorRed(email2Error);
+      hideErrorMessage(firstNameError);
+      hideErrorMessage(lastNameError);
+      hideErrorMessage(email1Error);
+      hideErrorMessage(password1Error);
+      hideErrorMessage(password2Error);
+      showErrorMessage(email2Error, "Please fill out this field.")
     } else if (password1Field.val() == '') {
       event.preventDefault();
+      changeColorRed(password1Error);
+      hideErrorMessage(firstNameError);
+      hideErrorMessage(lastNameError);
+      hideErrorMessage(email1Error);
+      hideErrorMessage(email2Error);
+      hideErrorMessage(password2Error);
+      showErrorMessage(password1Error, "Please fill out this field.")
     } else if (password2Field.val() == '') {
       event.preventDefault();
+      changeColorRed(password2Error);
+      hideErrorMessage(firstNameError);
+      hideErrorMessage(lastNameError);
+      hideErrorMessage(email1Error);
+      hideErrorMessage(email2Error);
+      hideErrorMessage(password1Error);
+      showErrorMessage(password2Error, "Please fill out this field.")
     }
+    
+    
     
     
     
@@ -58,8 +102,12 @@ function showErrorMessage(fieldError, newText) {
   fieldError.parent().get(0).className = "d-block";
 };
 
-function changeColor(fieldError) {
+function changeColorRed(fieldError) {
   fieldError.get(0).className = "form-text text-danger";
+};
+
+function changeColorMuted(fieldError) {
+  fieldError.get(0).className = "form-text text-muted";
 };
 
 
--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,6 @@
 import FirebaseAuth
 from firebase_admin import auth
-
+#
 # user = auth.create_user(
 #     email='user@example.com',
 #     email_verified=False,
@@ -8,6 +8,19 @@ from firebase_admin import auth
 #     display_name='John Doe',
 #     disabled=False)
 # print('Successfully fetched user data: {0}'.format(user.uid))
+# print('Successfully fetched user data: {0}'.format(user))
+#
+uid = 'Ot43OM7k4mOhmU7QbNSDsEbInvY2'
 
-user = auth.get_user('Ot43OM7k4mOhmU7QbNSDsEbInvY2')
-print(user.photo_url)
+# user = auth.get_user(uid)
+# print(user.email)
+#
+# custom_token = auth.create_custom_token(uid)
+# print(custom_token)
+
+id_token = 'eyJ0eXAiOiAiSldUIiwgImFsZyI6ICJSUzI1NiIsICJraWQiOiAiYjdmYzQ5YjhjZjA2YmVhODk4YjM4YmU4MmFhOWExMDIyZjViNGJiMyJ9.eyJpc3MiOiAiZmlyZWJhc2UtYWRtaW5zZGstdHo0eG9AaG9tZW1hZGUtY3Vpc2luZS5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsICJzdWIiOiAiZmlyZWJhc2UtYWRtaW5zZGstdHo0eG9AaG9tZW1hZGUtY3Vpc2luZS5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsICJhdWQiOiAiaHR0cHM6Ly9pZGVudGl0eXRvb2xraXQuZ29vZ2xlYXBpcy5jb20vZ29vZ2xlLmlkZW50aXR5LmlkZW50aXR5dG9vbGtpdC52MS5JZGVudGl0eVRvb2xraXQiLCAidWlkIjogIk90NDNPTTdrNG1PaG1VN1FiTlNEc0ViSW52WTIiLCAiaWF0IjogMTU0Mzg5Mzg5NSwgImV4cCI6IDE1NDM4OTc0OTV9.u-XlJEEDGoaMcYQuppl3jyxBkP515mYhK87VZXz0S3DFrnmkuaz0OKNgErmWBcdiirR86p01k6zmZHkLKQDA4XCltrncmCh8IhuHN6LJAV6zkRBqqn_BQLpiT16IXH6dasFoB5V-9p5zE36WnoXeL7r1EYjGc9bHFY6Bfzn6PxUcXAV1VM40yTGHj3KgjzquoZnmX_npIP2Sn5U6BqkJ9mRZ76QILfDdXp6f-H8oKJA-8tzRz_1WZ54qGY6_2dL98BDUpUIDNU2OIIjUh6pOmk5zgwbxcDgAGlP2GtkYUHXG6yAHhJq7RfCoJnIxqYuqAxxFGnn14VbvU_kd5bC6Kg=='
+
+decoded_token = auth.verify_id_token(id_token)
+uid = decoded_token['uid']
+print(uid)
+

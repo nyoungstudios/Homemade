f3690eed2294d991fcc8c02b9b5c00b22fe2e4d0
nyoungstudios <nathanielyoung95070@gmail.com>
2018-12-05 02:39:36 -0500

    links profile picture with user

--- backend/static/profile.js
+++ backend/static/profile.js
@@ -14,7 +14,7 @@ function uploadProfPic() {
   var preview = $('#myProfImg'); 
   var image = $("input[type='file']").get(0).files[0];
   var reader = new FileReader();  
-  var storageRefProf = storageRef.child('profilePictures/' + image.name);
+  var storageRefProf = storageRef.child('profilePictures/' + new Date().getTime() + image.name);
   
   reader.onload = function() {
     preview.get(0).src = reader.result;
@@ -27,6 +27,16 @@ function uploadProfPic() {
   storageRefProf.put(image).then(function(snapshot) {
     snapshot.ref.getDownloadURL().then(function(downloadURL) {
       console.log(downloadURL);
+      var user = firebase.auth().currentUser;
+
+      user.updateProfile({
+        photoURL: downloadURL
+      }).then(function() {
+        // Update successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
+      
     });
   });
   

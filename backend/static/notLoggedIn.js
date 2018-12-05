3bbfccdc10e879513219556cfe776f6e5577ae77
nyoungstudios <nathanielyoung95070@gmail.com>
2018-12-04 22:10:52 -0500

    profile js file

--- backend/static/notLoggedIn.js
+++ backend/static/notLoggedIn.js
@@ -1,8 +1,13 @@
 //javascript file for already logged in
+
+var name, photoUrl;
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
     document.getElementsByTagName('body')[0].className = 'd-block';
+    name = user.displayName;
+    photoUrl = user.photoURL;
   } else {
     window.location = '/signup';
   }
---
+++ backend/static/profile.js
@@ -0,0 +1,6 @@
+//javascript file for login webpage
+
+$(document).ready(function() {
+  console.log(name);
+  console.log(photoUrl);
+});
